fff65718a6bd3ed1811b07bb638e61fe5f545c6a
Jith Karingada <jith.karingada@gmail.com>
2025-11-02 06:39:10 -0500

    updated with SetDefaults

--- psqldb/config.go
+++ psqldb/config.go
@@ -8,6 +8,34 @@ type ConnectionPoolConfig struct {
 	HealthCheckPeriodInSec int
 }
 
+func (c *ConnectionPoolConfig) SetDefaults() {
+	if c.MaxOpenConns == 0 {
+		c.MaxOpenConns = 25
+	}
+	if c.MaxIdleConns == 0 {
+		c.MaxIdleConns = 25
+	}
+	if c.ConnMaxLifetimeInHours == 0 {
+		c.ConnMaxLifetimeInHours = 1
+	}
+	if c.ConnMaxIdleTimeInMin == 0 {
+		c.ConnMaxIdleTimeInMin = 15
+	}
+	if c.HealthCheckPeriodInSec == 0 {
+		c.HealthCheckPeriodInSec = 30
+	}
+}
+
+func (c ConnectionPoolConfig) IsValid() bool {
+	if c.MaxOpenConns < 0 || c.MaxIdleConns < 0 {
+		return false
+	}
+	if c.ConnMaxLifetimeInHours < 0 || c.ConnMaxIdleTimeInMin < 0 || c.HealthCheckPeriodInSec < 0 {
+		return false
+	}
+	return true
+}
+
 type Config struct {
 	ApplicationRWDatabaseURL              string
 	ApplicationSchemaMigrationDatabaseUrl string
